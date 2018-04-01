1988823e04dc85cca300ca6124e8aeaf0968fe12
Ronald Sianturi <rsianturi77@gmail.com>
2018-04-01 17:08:12 +0700

    push

---
+++ app/src/androidTest/java/id/android/RONALD_1202150002_MODUL6/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package id.android.RONALD_1202150002_MODUL6;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("id.odt.KHRISNA_1202154145_MODUL6", appContext.getPackageName());
+    }
+}
